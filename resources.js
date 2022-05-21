703f1fd14ce66fa6d030bf2409b2d55aff017bb0
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2022-05-20 19:28:28 -0500

    updated

--- resources.js
+++ resources.js
@@ -7,29 +7,50 @@ exports.resources = {
                 "id": 1,
                 "name": "Scrimba",
                 "url": "https://scrimba.com/",
-                "content": "technology",
+                "content": "educational",
                 "free": false
             },
             {
                 "id": 2,
                 "name": "freeCodeCamp",
                 "url": "https://www.freecodecamp.org/",
-                "content": "technology",
+                "content": "educational",
                 "free": true
             },
             {
                 "id": 3,
                 "name": "codeAcademy",
                 "url": "https://www.codecademy.com/",
-                "content": "technology",
+                "content": "educational",
                 "free": false
             },
             {
                 "id": 4,
-                "name": "FAFSA",
+                "name": "ACC Grants and Scholarships",
+                "url": "https://austincc.academicworks.com/ ",
+                "content": "finance",
+                "free": false
+            },
+            {
+                "id": 5,
+                "name": "Fafsa",
                 "url": "https://studentaid.gov/h/apply-for-aid/fafsa",
                 "content": "finance",
                 "free": false
+            },
+            {
+                "id": 6,
+                "name": "Investopedia",
+                "url": "https://www.investopedia.com/articles/basics/06/invest1000.asp",
+                "content": "finance",
+                "free": true
+            },
+            {
+                "id": 7,
+                "name": "First Generation College Student Assistance",
+                "url": "https://firstgen.naspa.org/journal-and-research/national-data-fact-sheets-on-first-generation-college-students/national-data-fact-sheets",
+                "content": "finance",
+                "free": true
             }
         ]
     }
